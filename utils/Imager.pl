03627a8c1f639134a3aca3a3371fde7a82ec50b3
marisa <marisa@Zidane.localdomain>
2010-10-19 16:50:56 +0700

    Imager_v1

---
+++ utils/Imager.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+@a=`find ./ | grep -i "tga"`;
+
+$prog="~/work/unlzss/bin/Release/unlzss";
+
+for ($i=0; $i<=$#a; $i++)
+{
+	chomp($a[$i]);
+        $ind=rindex($a[$i],"/");
+	$subcp=substr($a[$i],$ind+1);
+	$ind=rindex($a[$i],".");
+	$fname=substr($a[$i],0,$ind);
+	#print $subcp."\n";
+
+	$pss=`grep -m 1 -r -i "$subcp" ./SCRIPTS/`;
+	chomp($pss);
+	@b=split(/:/,$pss);
+
+	
+	
+	if ($#b > -1)
+	{
+	@c=`grep pana "$b[0]"`;
+	system("$prog $a[$i] ");
+	
+	if ($#c >-1)
+	   {
+		system("convert -transpose $a[$i] $fname.png");
+	   }
+	else
+	   {
+		system("convert $a[$i] $fname.png");
+	   }
+	#print $b[0]." $c[1]"."\n";
+	}
+	else
+	{
+	print $subcp."\n";
+	}
+	
+}
+
